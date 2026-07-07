888fb1d9940938b3031442078e68bf3a7337ae67
artem <jock.fromrussia@gmail.com>
2026-07-07 21:02:35 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const seatsContainer = document.querySelector(".cinema__seats");
-const movies = document.querySelector("#movies");
-const countSelectedSeats = document.querySelector("#count");
-const totalPrice = document.querySelector("#total");
+"use strict";
 
-let ticketPrice = Number(movies.value);
+// dom-элементы
+const cinemaHall = document.querySelector(".cinema__hall");
+const movieSelect = document.querySelector("#movie-select");
+const selectedSeatsCount = document.querySelector("#selected-seat-count");
+const totalPrice = document.querySelector("#total-price");
 
+// вспомогательные функции
 function isFreeSeat(element) {
-	return (
-		element.classList.contains("seat") &&
-		!element.classList.contains("seat--occupied")
-	);
+	const isSeat = element.classList.contains("seat");
+	const isFree = !element.classList.contains("seat--occupied");
+
+	return isSeat && isFree;
 }
 
-function painSeat(seat) {
+function toggleSelectedSeat(seat) {
 	seat.classList.toggle("seat--selected");
 }
 
-function calculateCountSelectedSeats() {
+function calculateSelectedSeatsCount() {
 	const selectedSeats = document.querySelectorAll(
 		".cinema__row .seat--selected",
 	);
@@ -25,26 +27,22 @@ function calculateCountSelectedSeats() {
 }
 
 function updateReceipt() {
-	const count = calculateCountSelectedSeats();
-	countSelectedSeats.textContent = count;
+	const count = calculateSelectedSeatsCount();
 
-	updateTicketPrice();
-	totalPrice.textContent = count * ticketPrice;
+	selectedSeatsCount.textContent = count;
+	totalPrice.textContent = count * Number(movieSelect.value);
 }
 
-function updateTicketPrice() {
-	ticketPrice = Number(movies.value);
-}
-
-function selectSeat(event) {
+// Основные функции
+function handleSeatClick(event) {
 	const target = event.target;
 
 	if (isFreeSeat(target)) {
-		painSeat(target);
+		toggleSelectedSeat(target);
 		updateReceipt();
 	}
 }
 
-seatsContainer.addEventListener("click", selectSeat);
+cinemaHall.addEventListener("click", handleSeatClick);
 
 // ошибка: есть возможность выбрать разные места в разных фильмах
