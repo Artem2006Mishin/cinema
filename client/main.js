dbe5a8ebd9d06c2fdfbcf0a1cfedcfe0f465907d
artem <jock.fromrussia@gmail.com>
2026-07-15 15:02:06 +0300

    made init storage

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
 import { initMovies } from "./utils/movies.js";
 import { initCinema } from "./utils/cinema.js";
 import { initReservation } from "./utils/reservation.js";
+import { saveToStorage } from "./utils/storage.js";
+
+const $initStorageBtn = document.querySelector("#init-storage");
+$initStorageBtn.addEventListener("click", () => {
+	saveToStorage("optionId", "avengers-endgame");
+});
 
 async function main() {
 	await initMovies();
